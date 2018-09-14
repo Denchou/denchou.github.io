1601fdcf074cbd6e11c40be415147ec562db0301
denchou <denchou617@gmail.com>
2018-09-14 00:30:02 -0400

    Finished

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,3 +1,16 @@
 $(() => {
-
-})
+  $('#ttt').hide()
+  $('#yo').hide()
+  $('.message').hide()
+  $('.tic-tac-toe').on('mouseenter', () => {
+    $('#ttt').show()
+    $('.message').hide()
+  })
+  $('.tic-tac-toe').on('mouseleave', () => $('#ttt').hide())
+  $('.your-opinion').on('mouseenter', () => {
+    $('#yo').show()
+    $('.message').hide()
+  })
+  $('.your-opinion').on('mouseleave', () => $('#yo').hide())
+  $('#about-me').on('click', () => $('.message').show())
+}).foundation()
