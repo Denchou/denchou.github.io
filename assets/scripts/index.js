9bdeda957fa1d8439f127bd7f2ec690b795f3870
denchou <denchou617@gmail.com>
2018-09-14 00:59:23 -0400

    Template 1

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,16 +2,25 @@ $(() => {
   $('#ttt').hide()
   $('#yo').hide()
   $('.message').hide()
+  $('.skill').hide()
   $('.tic-tac-toe').on('mouseenter', () => {
     $('#ttt').show()
     $('.message').hide()
+    $('.skill').hide()
   })
   $('.tic-tac-toe').on('mouseleave', () => $('#ttt').hide())
   $('.your-opinion').on('mouseenter', () => {
     $('#yo').show()
     $('.message').hide()
+    $('.skill').hide()
   })
   $('.your-opinion').on('mouseleave', () => $('#yo').hide())
-  $('#about-me').on('click', () => $('.message').show())
-  $('#skills').on('click', () => $('.skill').show())
+  $('#about-me').on('click', () => {
+    $('.message').show()
+    $('.skill').hide()
+  })
+  $('#skills').on('click', () => {
+    $('.skill').show()
+    $('.message').hide()
+  })
 }).foundation()
